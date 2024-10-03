8e8794484d4d8942d122ccd196131584e892ca11
Rita Ritina <ritinarita559@gmail.com>
2024-10-03 12:48:53 +0300

    Implementing authorization via ReduxToolkit

--- src/main.tsx
+++ src/main.tsx
@@ -12,6 +12,8 @@ import AuthLayouth from './layout/Auth/AuthLayout.tsx';
 import Login from './pages/Login/Login.tsx';
 import Register from './pages/Register/Register.tsx';
 import RequireAuth from './helpers/RequireAuth.tsx';
+import { Provider } from 'react-redux';
+import { store } from './store/store.ts';
 
 const Menu = lazy(() => import('./pages/Menu/Menu'));
 
@@ -93,6 +95,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')!).render(
    <StrictMode>
-      <RouterProvider router={router} />
+      <Provider store={store}>
+         <RouterProvider router={router} />
+      </Provider>
    </StrictMode>,
 );
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -7,6 +7,9 @@ import { FormEvent, useState } from 'react';
 import axios, { AxiosError } from 'axios';
 import { PREFIX } from '../../helpers/API';
 import { LoginResponce } from '../../interfaces/authInterface';
+import { useDispatch } from 'react-redux';
+import { AppDispath } from '../../store/store';
+import { userActions } from '../../store/user.slice';
 
 export interface LoginForm {
 	email: {
@@ -20,6 +23,7 @@ export interface LoginForm {
 const Login = () => {
 const [ error, setError ] = useState<string | null>();
 const navigate = useNavigate();
+const dispatch = useDispatch<AppDispath>();
 
    const submit =  async (e: FormEvent) => {
       e.preventDefault();
@@ -35,7 +39,7 @@ const navigate = useNavigate();
 				email,
 				password,
 			});
-			localStorage.setItem('jwt', data.access_token);
+			dispatch(userActions.addJwt(data.access_token));
 			navigate('/');
 		} catch (e) {
 			if(e instanceof AxiosError) {
