b7559e9c7b2d01802e3b1c64ad37c1be729f2bad
Rita Ritina <ritinarita559@gmail.com>
2024-10-11 19:03:11 +0300

    Зrice generation

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -7,34 +7,101 @@ import axios from 'axios';
 import { PREFIX } from '../../helpers/API';
 import { Product } from '../../interfaces/product.interface';
 import { useEffect, useState } from 'react';
+import Input from '../../components/Input/Input';
+import Button from '../../components/Button/Button';
+
+const  DELIVERY_PRICE = 169;
 
 const Cart = () => {
-	const [cartPproducts, setCartProducts] = useState<Product[]>();
-	const items = useSelector((s: RootState) => s.cart.items);
-	useEffect(() => {
-		loadALLItems();
-	}, [items]);
+   const [cartPproducts, setCartProducts] = useState<Product[]>();
+   const items = useSelector((s: RootState) => s.cart.items);
 
-	const getItem = async (id: number) => {
-		const { data } = await axios.get<Product>(`${PREFIX}/products/${id}`);
-		return data;
-	};
+   console.log(cartPproducts);
 
-	const loadALLItems = async () => {
-		const res = await Promise.all(items.map(i => getItem(i.id)));
-		setCartProducts(res);
-	};
+   useEffect(() => {
+      loadALLItems();
+   }, [items]);
+
+   const getItem = async (id: number) => {
+      const { data } = await axios.get<Product>(`${PREFIX}/products/${id}`);
+      return data;
+   };
+
+   const loadALLItems = async () => {
+      const res = await Promise.all(items.map((i) => getItem(i.id)));
+      setCartProducts(res);
+   };
+
+   const getPrice = () => {
+      const prices: number[] = [];
+
+      items.forEach((i) => {
+         const product = cartPproducts?.find((p) => {
+            if (p.id === i.id) {
+               return p;
+            }
+         });
+
+         if (product) {
+            prices.push(product.price * i.count);
+         }
+      });
+
+      return prices.reduce((acc, p) => acc + p, 0);
+   };
 
    return (
       <div className={styles.container}>
-         <Title >Корзина</Title>
+         <Title>Корзина</Title>
          {items.map((i) => {
-            const product = cartPproducts?.find(p => p.id === i.id);
-				if(!product) {
-					return;
-				}
-				return <CartItem key={product.id} count={i.count} {...product}></CartItem>;
+            const product = cartPproducts?.find((p) => p.id === i.id);
+            if (!product) {
+               return;
+            }
+            return (
+               <CartItem
+                  key={product.id}
+                  count={i.count}
+                  {...product}
+               ></CartItem>
+            );
          })}
+         <div className={styles.promoCode}>
+            <Input
+               maxLength={20}
+               className={styles.input}
+               placeholder="Промокод"
+            />
+            <Button className={styles.button}>Применить</Button>
+         </div>
+         <div className={styles.info}>
+            <div className={styles.meaning}>
+               <span className={styles.text}>Итог</span>
+               <span className={styles.price}>
+                  {getPrice()}
+                  <span>₽</span>
+               </span>
+            </div>
+            <div className={styles.meaning}>
+               <span className={styles.text}>Доставка</span>
+               <span className={styles.price}>
+                  {DELIVERY_PRICE}
+                  <span>₽</span>
+               </span>
+            </div>
+            <div className={styles.meaning}>
+               <span className={styles.text}>
+                  Итог<span>({items.length})</span>
+               </span>
+               <span className={styles.price}>
+                  {getPrice() + DELIVERY_PRICE}
+                  <span>₽</span>
+               </span>
+            </div>
+         </div>
+         <div className={styles.orderButton}>
+            <Button onClick={} appearence="big">Оформить</Button>
+         </div>
       </div>
    );
 };
